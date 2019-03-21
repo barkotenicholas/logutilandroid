e60115b8b48382a9d04ce3615ff945f7fdf98940
deathsparrow101 <barkotenicholas@gmail.com>
2019-03-21 12:22:32 +0300

    Create project in studio

---
+++ logutil2/src/main/java/com/example/logutil2/LogDebug.java
@@ -0,0 +1,14 @@
+package com.example.logutil2;
+
+import android.util.Log;
+
+public class LogDebug {
+
+
+    private static final String TAG = "SUPER_AWESOME_APP";
+
+
+    public static void  d(String message){
+        Log.d(TAG,message);
+    }
+}
